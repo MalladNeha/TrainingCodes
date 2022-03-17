ab8d7799e11cbd8303be3ff60fa97b0c8a5e735b
MalladNeha <101259418+MalladNeha@users.noreply.github.com>
2022-03-17 12:04:02 +0530

    Add files via upload

---
+++ AssignmentsTraining/Ascii.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	char a;
+	cout << "\n enter any character :";
+	cin >> a;
+	
+	int b = (int)a;
+
+	cout << "\n Ascii value :" << b;
+	return 0;
+}
